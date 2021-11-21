af631bef0c9188ebe07b7bbd2459dc4cdf1fb119
Abdelouahedd <ennouriabdelouahed@gmail.com>
2021-11-21 19:20:18 +0100

    feat(save); create end point to lunch job for saving list of employees

--- src/main/java/com/example/springbatchdemo/web/EmployeeController.java
+++ src/main/java/com/example/springbatchdemo/web/EmployeeController.java
@@ -1,18 +1,27 @@
 package com.example.springbatchdemo.web;
 
-import com.example.springbatchdemo.service.EmpService;
 import com.example.springbatchdemo.domain.Employee;
+import com.example.springbatchdemo.service.EmpService;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api")
 public class EmployeeController {
     private final EmpService empService;
-
+    private final JobLauncher jobLauncher;
+    private final Job job;
 
     @PostMapping("/employee")
     public Employee saveEmp(Employee employee) {
@@ -32,6 +41,14 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
         return this.empService.getAllEmployees();
+    }
+
+    @GetMapping("/employees/job")
+    @SneakyThrows
+    public void runJob() {
+        Map<String, JobParameter> params = new HashMap<>();
+        params.put("saveDate", new JobParameter(new Date()));
+        jobLauncher.run(job, new JobParameters(params));
     }
 
 }
