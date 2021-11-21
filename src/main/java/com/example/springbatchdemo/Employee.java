90f29183d8e4f15a12879098c225f0b6ea490120
Abdelouahedd <ennouriabdelouahed@gmail.com>
2021-11-21 15:01:10 +0100

    feat(model) create entity Employee

---
+++ src/main/java/com/example/springbatchdemo/Employee.java
@@ -0,0 +1,25 @@
+package com.example.springbatchdemo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "table")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String gender;
+    private String ipAddress;
+}
